b103271b5d33fedbd4744dad9a470df1d3e1029f
Blacklight <bilalabdlhamid6@gmail.com>
2024-12-12 17:21:35 +0100

    Simple Dijkstra

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,35 +1,122 @@
+import java.util.*;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Dijkstra {
 
     public static void main(String[] args) {
+        List<Knoten> knoten = new ArrayList<>();
 
-        Scanner in = new Scanner(System.in);
+        Knoten knotenA = new Knoten("A");
+        knoten.add(knotenA);
 
-        int n = in.nextInt();
-        int[][] graph = new int[n+1][n+1];
+        Knoten knotenB = new Knoten("B");
+        knoten.add(knotenB);
 
-        int m = in.nextInt();
-        for (int i = 0; i < m; i++) {
-            int a, b;
-            a = in.nextInt();
-            b = in.nextInt();
+        Knoten knotenC = new Knoten("C");
+        knoten.add(knotenC);
 
-            graph[a][b] = 1;
-            graph[b][a] = 1;
-        }
+        Knoten knotenD = new Knoten("D");
+        knoten.add(knotenD);
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print(graph[i][j] + " ");
+        Knoten knotenE = new Knoten("E");
+        knoten.add(knotenE);
+
+        Knoten knotenF = new Knoten("F");
+        knoten.add(knotenF);
+
+        Knoten knotenG = new Knoten("G");
+        knoten.add(knotenG);
+
+        knotenA.verbindeZu(knotenB, 20);
+        knotenA.verbindeZu(knotenC, 30);
+
+        knotenB.verbindeZu(knotenD, 25);
+
+        knotenC.verbindeZu(knotenB, 10);
+        knotenC.verbindeZu(knotenD, 5);
+
+        knotenD.verbindeZu(knotenE, 10);
+        knotenD.verbindeZu(knotenF, 25);
+
+        knotenE.verbindeZu(knotenB, 15);
+        knotenE.verbindeZu(knotenF, 10);
+
+        knotenF.verbindeZu(knotenG, 5);
+
+        dijkstra(knotenA, knotenG);
+    }
+
+    static void dijkstra(Knoten start, Knoten ziel) {
+        Queue<Knoten> pq = new PriorityQueue<>();
+        Set<Knoten> besuchteKnoten = new HashSet<>();
+
+        start.kosten = 0;
+        pq.add(start);
+
+        while (!pq.isEmpty()) {
+            Knoten aktuellerKnoten = pq.poll();
+
+            if(besuchteKnoten.contains(aktuellerKnoten))
+                continue;
+
+            for(Kante kante : aktuellerKnoten.kanten) { //Iteriere durch alle Nachbarn
+                if( (aktuellerKnoten.kosten + kante.gewicht) < kante.verbundenerKnoten.kosten ) {
+                    kante.verbundenerKnoten.kosten = aktuellerKnoten.kosten + kante.gewicht;
+                    kante.verbundenerKnoten.vorgaenger = aktuellerKnoten;
+                    pq.add(kante.verbundenerKnoten);
+                }
             }
-            System.out.println(" ");
+
+            besuchteKnoten.add(aktuellerKnoten);
         }
+
+        Knoten aktuellerKnoten = ziel;
+        StringBuilder weg = new StringBuilder();
+
+        while(aktuellerKnoten.vorgaenger != null) {
+            weg.append(aktuellerKnoten.wert);
+            aktuellerKnoten = aktuellerKnoten.vorgaenger;
+        }
+
+        if(aktuellerKnoten != start)
+            System.out.println("Es gibt keinen Weg zum Knoten " + ziel.wert);
+        else {
+            weg.append(start.wert);
+            System.out.println("Kürzester Weg: " + weg.reverse() + " mit den insgesamten Kosten von " + ziel.kosten);
+        }
+
 
     }
 
-    static void dijkstra(int startV, int[][] graph) {
+}
 
+class Knoten implements Comparable<Knoten> {
+    String wert;
+    Knoten vorgaenger = null;
+    int kosten = Integer.MAX_VALUE;
+    List<Kante> kanten = new ArrayList<>();
+
+    public Knoten(String wert) {
+        this.wert = wert;
+    }
+
+    void verbindeZu(Knoten knoten, int gewicht) {
+        kanten.add(new Kante(knoten, gewicht));
+    }
+
+    @Override
+    public int compareTo(Knoten other) {
+        return Integer.compare(this.kosten, other.kosten);
+    }
+
+}
+
+class Kante {
+    int gewicht;
+    Knoten verbundenerKnoten;
+
+    public Kante(Knoten knoten, int gewicht) {
+        this.verbundenerKnoten = knoten;
+        this.gewicht = gewicht;
     }
 }
